6cf5924222d035ed4a639fe373b1d1b621d83075
Ali Abidi <mac_universe@live.com>
2020-12-30 23:27:33 +0530

    Type and assign variable

--- type.go
+++ type.go
@@ -8,6 +8,8 @@ var x = `this is also string's`
 var k = `"This is multiple", 
 string`
 
+var l string
+
 func main() {
 	fmt.Println(y)
 	fmt.Printf("%T\n", y)
@@ -17,6 +19,9 @@ func main() {
 	fmt.Printf("%T\n", x)
 	fmt.Println(k)
 	fmt.Printf("%T\n", k)
+	l = "james bond"
+	fmt.Println(l)
+	fmt.Printf("%T\n", l)
 	//z = 100
 	//fmt.Println(z)
 }
