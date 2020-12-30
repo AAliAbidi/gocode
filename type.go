a055c7b2d4719b1c7c85c4767a8c77cca86b9b9a
Ali Abidi <mac_universe@live.com>
2020-12-30 23:23:26 +0530

    The aggrigate datatype

---
+++ type.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var y = 42
+var z = "Samasung Galaxy"
+var x = `this is also string's`
+var k = `"This is multiple", 
+string`
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Println(k)
+	fmt.Printf("%T\n", k)
+	//z = 100
+	//fmt.Println(z)
+}
